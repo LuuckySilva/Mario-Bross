e3db8ff1de7858d1812c7c390fa4a92917c8ade8
Lucas <lukasantonyo@gmail.com>
2026-04-25 10:01:51 -0300

    Fix responsiveness, form behavior and background video

--- scripts.js
+++ scripts.js
@@ -1,17 +1,14 @@
-const form = document.querySelector(".fale-conosco")
-const mascara =document.querySelector(".mascara-formulario")
+const form = document.querySelector(".fale-conosco");
+const mascara = document.querySelector(".mascara-formulario");
 
-function mostrarform(){
-    
-    
-    form.style.left ="50%"
-    form.style.fransform = "translatex(-50%)"
-    mascara.style.visibility="visible"
+function mostrarform() {
+    form.style.left = "50%";
+    form.style.transform = "translateX(-50%)";
+    mascara.style.visibility = "visible";
 }
 
-function esconderform(){
-    form.style.left ="-300px"
-    form.style.fransform = "translatex(0)"
-    mascara.style.visibility="hidden"
-
+function esconderform() {
+    form.style.left = "-300px";
+    form.style.transform = "translateX(0)";
+    mascara.style.visibility = "hidden";
 }
